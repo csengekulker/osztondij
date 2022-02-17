be5a0e4af1ac23cd68a0081a603ebbfe3dd40ebb
csengekulker <k71608883429@ktch.hu>
2022-02-17 20:08:30 +0100

    fixed run.sh typo, index can be calculated

---
+++ Osztondij.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Osztondij {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Balogh Csenge\nSzoft_I_N\n2022.02.17");
+
+    System.out.print("Elért kreditpont: ");
+    int creditPoints = sc.nextInt();
+
+    System.out.print("Osztályzatok átlaga: ");
+    double gradesMean = sc.nextDouble();
+
+    double index = creditPoints * gradesMean / 30;
+
+    System.out.println("Ösztöndíjindexe: " + index); 
+  }
+}
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Balogh Csenge
+# Szoft_I_N
+# 2022.02.17
+
+javac Osztondij.java
+
+java Osztondij
+
+echo "Press Enter to exit"
+
+read
